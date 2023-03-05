53457cbaff8001ec8f88e1727ea0b122b651bfbe
Ahsan <fa19-bse-059@cuiatk.edu.pk>
2023-03-05 18:00:45 +0500

    add Role Migration file and table

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const { port } = require("./config/server");
 const apiRoutes = require("./routes/index");
 const bodyParser = require("body-parser");
 
+const db = require("./models/index");
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,6 +14,10 @@ app.use("/api", apiRoutes);
 const startServer = () => {
   app.listen(port, () => {
     console.log(`Server started on port:${port}`);
+
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
   });
 };
 
--- src/models/user.js
+++ src/models/user.js
@@ -7,13 +7,10 @@ const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      User.belongsToMany(models.Role, {
+        through: "User_Roles",
+      });
     }
   }
   User.init(
