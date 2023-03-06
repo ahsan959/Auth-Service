d8e7432f4c26e779c9e3657b2154d0ea0d44d3de
ahsan <ahan@gmail.com>
2023-03-06 14:30:27 +0500

    added getuser which have customer role and admin role

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ const apiRoutes = require("./routes/index");
 const bodyParser = require("body-parser");
 
 const db = require("./models/index");
+const { User, Role } = require("./models/index");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/api", apiRoutes);
 const startServer = () => {
-  app.listen(port, () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
+  app.listen(port, async () => {
     console.log(`Server started on port:${port}`);
 
     if (process.env.DB_SYNC) {
       db.sequelize.sync({ alter: true });
     }
+
+    const u1 = await User.findByPk(4);
+    const r1 = await Role.findByPk(2);
+    // u1.addRole(r1);
+
+    // getRoles
+    const response = await r1.getUsers();
+    console.log(response);
   });
 };
 
